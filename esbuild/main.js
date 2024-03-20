df28d1dd27d31217a751e0c65b9f276d83828aa9
Svdex <ucihaibna@yahoo.com>
2024-03-20 15:37:15 +0700

    Initialize repository

---
+++ esbuild/main.js
@@ -0,0 +1,27 @@
+const esbuild = require("esbuild");
+const z = require("zod");
+
+const { entryPoint, outfile, minify } = z
+  .object({
+    entryPoint: z.string().default("./data/scripts/main.ts"),
+    outfile: z.string().default("./BP/scripts/main.js"),
+    minify: z.boolean().default(true),
+  })
+  .parse(JSON.parse(process.argv[2] ?? "{}"));
+
+(async () => {
+  try {
+    await esbuild.build({
+      bundle: true,
+      entryPoints: [entryPoint],
+      external: ["@minecraft/server", "@minecraft/server-ui"],
+      format: "esm",
+      outfile,
+      minify,
+    });
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    esbuild.stop();
+  }
+})();
