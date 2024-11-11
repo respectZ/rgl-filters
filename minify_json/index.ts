8759fd0536acb5df9c7d4afabe416d1f7518a707
Svdex <renixiel@gmail.com>
2024-11-12 04:07:27 +0700

    fix: change parser to lossless

--- minify_json/index.ts
+++ minify_json/index.ts
@@ -1,13 +1,26 @@
 import fg from "npm:fast-glob@3.2.0";
-import jsonc from "npm:jsonc-parser@3.0.0";
+import { parse, stringify } from "npm:lossless-json@4.0.2";
+
+const commentRegex = /(\/\*.*\*\/)|(\/\/[^\n]*)/gs;
+
+async function loadJson(filepath: string) {
+  const content = await Deno.readTextFile(filepath);
+  const data = content.replaceAll(commentRegex, (match) => {
+    return " ".repeat(match.length);
+  });
+  const json = parse(data);
+  return json;
+}
 
 const dirs = ["./BP/**/*.json", "./RP/**/*.json"];
 const entries = await fg(dirs);
 await Promise.all(
   entries.map(async (entry) => {
-    const content = await Deno.readTextFile(entry);
-    const json = jsonc.parse(content);
-    const minified = JSON.stringify(json);
+    const json = await loadJson(entry);
+    const minified = stringify(json);
+    if (!minified) {
+      throw new Error(`Failed to minify ${entry}`);
+    }
     await Deno.writeTextFile(entry, minified);
   })
 );
